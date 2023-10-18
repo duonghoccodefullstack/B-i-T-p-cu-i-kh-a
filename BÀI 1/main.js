1c4fa2209294c6254232a307792368e55997fadd
Bui Van Hai <haibv1377@dhcnhn.edu.vn>
2023-10-18 23:59:28 +0700

    first commit

---
+++ BÀI 1/main.js
@@ -0,0 +1,43 @@
+// let  inputNumberA = document.getElementById("numberA")
+// const inputNumberB = document.getElementById("numberB");
+// function click() {
+//   document.writeln(inputNumberA)
+// }click()
+
+
+// function sum() {
+//   let a = document.getElementById("numberA").value;
+//   let b = document.getElementById("numberB").value;
+
+//   // Kiểm tra dữ liệu
+//   if (a == " " || b == " ") {
+//     alert("Vui lòng nhập vào hai số");
+//     document.write(a);
+//     return false;
+//   }
+//   console.log(a);
+// }
+
+
+// function click() {let n = parseInt(document.getElementById("numberA").value)
+//     document.getElementById("Result").textContent = n
+//    if (n >= b) {
+//      alert("Vui lòng nhập a < b.");
+//      return;
+//    }
+// }
+    
+
+// function click() {
+//     var a = parseInt(document.getElementById("numberA").value);
+//     var b = parseInt(document.getElementById("numberB").value);
+//     var sum = 0;
+
+//     if (a >= b) {
+//         alert("Vui lòng nhập a < b.");
+//         return;
+//     }
+//     document.getElementById("Result").textContent= a  ;
+// }click();
+
+
